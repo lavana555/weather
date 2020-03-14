c7b1080e0263fae694b86af830612e2e8b85bdd8
lavana555 <timofeevniki3@gmail.com>
2020-03-14 19:06:27 +0300

    Updates

---
+++ precache-manifest.d047cc8aa9cd64cfc98842376d746aeb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8933fceb77325b8eec514c3ca91c042",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "016b3a7eafbadad6e903",
+    "url": "/weather/static/css/main.0ae9d16f.chunk.css"
+  },
+  {
+    "revision": "af3a081f1848622eb652",
+    "url": "/weather/static/js/2.95087aec.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather/static/js/2.95087aec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "016b3a7eafbadad6e903",
+    "url": "/weather/static/js/main.ca56fd38.chunk.js"
+  },
+  {
+    "revision": "21baee1ee3fded119a76",
+    "url": "/weather/static/js/runtime-main.6859beea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aef9109e5172dc2e7c9c79d2ae7e1669",
+    "url": "/weather/static/media/videoplayback.aef9109e.mp4"
+  },
+  {
+    "revision": "485afeba2f8c6e670dbb21afaf951473",
+    "url": "/weather/static/media/Футаж. Магическая пыль. Footage..485afeba.mp4"
+  }
+]);
